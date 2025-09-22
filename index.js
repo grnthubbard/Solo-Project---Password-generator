3ac06f1f0c7adbd77b72f7a9cf487ebee536c5b6
grnthubbard <granthubbard02@gmail.com>
2025-09-22 15:41:45 -0400

    1.01 for ben

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const value = document.querySelector("#characterValue")
 const input = document.querySelector("#characters")
 let password = document.getElementById("password")
-let numbers = [0,1,2,3,4,5,6,7,8,9]
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+let bigArray = []
 value.textContent = input.value;
 input.addEventListener("input", (event) => {
+    password.textContent = "";
     value.textContent = event.target.value;
     for(i=0; i<input.value; i++) {
-        password.textContent = Math.floor(Math.random()*9)
+        const randomIndex = Math.floor(Math.random()* letters.length)
+        password.textContent += letters[randomIndex]
     }
     
 })
